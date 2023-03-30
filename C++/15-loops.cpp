72056dca39263bb79cf13579c42cd0635481f130
AnmolDh <73706789+AnmolDh@users.noreply.github.com>
2023-03-30 19:38:23 +0530

    dk sighh

--- C++/15-loops.cpp
+++ C++/15-loops.cpp
@@ -1,20 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int i = 0;
-    while (i < 5){
+    while (i < 5)
+    {
         cout << i << endl;
         i++;
     }
 
     int j = 6;
-    do {
+    do
+    {
         cout << j << endl;
         j++;
     } while (j < 10);
 
-    for (int k = 10; k <= 15; k++){
+    for (int k = 10; k <= 15; k++)
+    {
         cout << k << endl;
     }
     return 0;
