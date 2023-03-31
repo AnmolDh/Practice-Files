8268aeea8eec8db3c5b8b089410f3f6799ddb453
AnmolDh <73706789+AnmolDh@users.noreply.github.com>
2023-03-31 10:24:56 +0530

    loops

---
+++ JAVA/foreachloop.java
@@ -0,0 +1,11 @@
+package JAVA;
+
+public class foreachloop {
+    public static void main(String[] args) {
+        // For Each Loop
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        for (String i : cars) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ JAVA/forloop.java
@@ -0,0 +1,18 @@
+package JAVA;
+
+public class forloop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+
+        // Nested Outer Loop
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Outer: " + i);
+            // Nested Inner Loop
+            for (int j = 0; j < 5; j++) {
+                System.out.println("Inner: " + j);
+            }
+        }
+    }
+}
