cfadd15f9d82c2e0d74f34dc133e0e3bc4237c91
AnmolDh <73706789+AnmolDh@users.noreply.github.com>
2023-03-31 10:25:09 +0530

    array

---
+++ JAVA/array.java
@@ -0,0 +1,10 @@
+package JAVA;
+
+public class array {
+    public static void main(String[] args) {
+        int[] myNum = {10, 20, 30, 40};
+        for (int i = 0; i < myNum.length; i++) {
+            System.out.println(myNum[i]);
+        }
+    }
+}
