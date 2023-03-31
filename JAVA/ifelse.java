4c7a2b988a32542106af85f5fbced222cab60a90
AnmolDh <73706789+AnmolDh@users.noreply.github.com>
2023-03-31 09:38:07 +0530

    java conditions

---
+++ JAVA/ifelse.java
@@ -0,0 +1,14 @@
+package JAVA;
+
+public class ifelse {
+    public static void main(String[] args) {
+        int time = 22;
+        if (time < 10) {
+            System.out.println("Good morning.");
+        } else if (time < 18) {
+            System.out.println("Good day.");
+        } else {
+            System.out.println("Good evening.");
+        }
+    }
+}
