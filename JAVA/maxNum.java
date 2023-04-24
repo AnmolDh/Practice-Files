8be502b75cd9219f49fa57ef19c6abec8005c180
AnmolDh <73706789+AnmolDh@users.noreply.github.com>
2023-04-24 14:54:12 +0530

    array prog

---
+++ JAVA/maxNum.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class maxNum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = input.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        System.out.println(arr[n - 1]);
+    }
+}
