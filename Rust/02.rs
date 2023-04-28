683feb9e4cdc61b1332155bfedde706aa218bf97
AnmolDh <73706789+AnmolDh@users.noreply.github.com>
2023-04-28 21:34:19 +0530

    rs

---
+++ Rust/02.rs
@@ -0,0 +1,11 @@
+fn main() {
+   let company_string = "TutorialsPoint";  // string type
+   let rating_float = 4.5;                 // float type
+   let is_growing_boolean = true;          // boolean type
+   let icon_char = '♥';                    //unicode character type
+
+   println!("company name is:{}",company_string);
+   println!("company rating on 5 is:{}",rating_float);
+   println!("company is growing :{}",is_growing_boolean);
+   println!("company icon is:{}",icon_char);
+}
