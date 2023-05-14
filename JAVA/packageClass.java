798559da07caa0438e29b5c589a39e3506ddd5bc
AnmolDh <73706789+AnmolDh@users.noreply.github.com>
2023-05-14 18:13:30 +0530

    package

---
+++ JAVA/packageClass.java
@@ -0,0 +1,13 @@
+package packageClassFolder;
+
+public class packageClass {
+    public static void main(String[] args) {
+        System.out.println("huh? a package");
+    }
+}
+
+// javac packageClass.java
+// &&
+// javac -d . packageClass.java
+// &&
+// java packageClassFolder.packageClass
