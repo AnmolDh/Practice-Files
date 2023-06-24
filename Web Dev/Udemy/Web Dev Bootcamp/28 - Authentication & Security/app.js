233255fd2a54e1244b4a13998975b1ba1e877bb0
AnmolDh <73706789+AnmolDh@users.noreply.github.com>
2023-06-25 01:11:39 +0530

    auth level 2 - env

--- Web Dev/Udemy/Web Dev Bootcamp/28 - Authentication & Security/app.js
+++ Web Dev/Udemy/Web Dev Bootcamp/28 - Authentication & Security/app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -16,8 +17,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret = "huhLooksGood";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = mongoose.model("user", userSchema);
 
