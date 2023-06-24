c302acdd9d50c060b74b86828097116d84ca3598
AnmolDh <73706789+AnmolDh@users.noreply.github.com>
2023-06-25 01:39:09 +0530

    auth level 3

--- Web Dev/Udemy/Web Dev Bootcamp/28 - Authentication & Security/app.js
+++ Web Dev/Udemy/Web Dev Bootcamp/28 - Authentication & Security/app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -16,8 +16,6 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = mongoose.model("user", userSchema);
 
@@ -37,7 +35,7 @@ app.get("/login", function (req, res) {
 app.post("/register", function (req, res) {
     const user = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     user.save().then(function () {
         res.render("secrets");
@@ -51,7 +49,7 @@ app.post("/login", function (req, res) {
         { email: req.body.username }
     ).then(function (r) {
         if (r) {
-            if (r.password === req.body.password) {
+            if (r.password === md5(req.body.password)) {
                 res.render("secrets");
             }
             else {
