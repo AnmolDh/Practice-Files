468452c12125e5a5cafab4a6a6ca0650514a6d3c
AnmolDh <73706789+AnmolDh@users.noreply.github.com>
2023-06-25 12:45:44 +0530

    auth level 6

--- Web Dev/Udemy/Web Dev Bootcamp/28 - Authentication & Security/app.js
+++ Web Dev/Udemy/Web Dev Bootcamp/28 - Authentication & Security/app.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const findOrCreate = require("mongoose-findorcreate");
+const googleStrategy = require("passport-google-oauth20").Strategy;
 
 
 const app = express();
@@ -24,21 +26,55 @@ mongoose.connect("mongodb://localhost:27017/authDB");
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = mongoose.model("user", userSchema);
 
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+passport.deserializeUser(function (id, done) {
+    User.findById(id)
+    .then(function (user) {
+        done(null, user);
+    })
+    .catch(function (err) {
+        done(err);
+    });
+});
+
+
+passport.use(new googleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets"
+},
+    function (accessToken, refreshToken, profile, cb) {
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+            return cb(err, user);
+        });
+    })
+);
 
 
 app.get("/", function (req, res) {
     res.render("home");
 });
+
+app.get("/auth/google", passport.authenticate("google", { scope: ["profile"] }));
+
+app.get("/auth/google/secrets",
+    passport.authenticate("google",
+        { failureRedirect: "/login" }),
+    function (req, res) {
+        res.redirect("/secrets");
+})
 
 app.get("/register", function (req, res) {
     res.render("register");
@@ -58,8 +94,12 @@ app.get("/secrets", function (req, res) {
 });
 
 app.get("/logout", function (req, res) {
-    req.logout();
-    res.redirect("/");
+    req.logout(function (err) {
+        if (err) {
+            console.log(err);
+        }
+        res.redirect("/");
+    });
 });
 
 app.post("/register", function (req, res) {
