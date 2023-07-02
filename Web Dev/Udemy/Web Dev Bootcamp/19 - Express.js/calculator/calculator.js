81c795986da37cf7d7236dbd80a9a9ee5fb279c4
AnmolDh <73706789+AnmolDh@users.noreply.github.com>
2023-07-02 14:54:24 +0530

    calc

--- Web Dev/Udemy/Web Dev Bootcamp/19 - Express.js/calculator/calculator.js
+++ Web Dev/Udemy/Web Dev Bootcamp/19 - Express.js/calculator/calculator.js
@@ -11,7 +11,12 @@ app.get("/", function (req, res) {
 app.post("/", function (req, res) {
     var num1 = Number(req.body.num1);
     var num2 = Number(req.body.num2);
-    var result = num1 + num2;
+    if (req.body.hasOwnProperty("add")) {
+        var result = num1 + num2;
+    }
+    else {
+        var result = num1 - num2;
+    }
     res.send("the result is " + result);
 });
 
