4971a1024ee367c56d2badfefec6224bc2c6b4bb
AnmolDh <73706789+AnmolDh@users.noreply.github.com>
2023-08-27 11:23:43 +0530

    java patterns

--- Languages/JAVA/re-JAVA/src/Patterns.java
+++ Languages/JAVA/re-JAVA/src/Patterns.java
@@ -41,7 +41,55 @@ public class Patterns {
 //            System.out.println();
 //        }
 
-//        inverted half pyramid (rotated by 180deg)
-        
+////        inverted half pyramid (rotated by 180deg)
+//        for (int i = 0; i < n; i++){
+//            for (int j = 0; j < n-i-1; j++){
+//                System.out.print(" ");
+//            }
+//            for (int j = 0; j < i+1; j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+////        half pyramid with numbers
+//        for (int i = 1; i <= n; i++){
+//            for (int j = 1; j <= i; j++){
+//                System.out.print(j + " ");
+//            }
+//            System.out.println();
+//        }
+
+////        inverted half pyramid with numbers
+//        for (int i = n; i > 0; i--){
+//            for (int j = 1; j <= i; j++){
+//                System.out.print(j + " ");
+//            }
+//            System.out.println();
+//        }
+
+////        floyd's triangle
+//        int k = 1;
+//        for (int i = 1; i <= n; i++){
+//            for (int j = 1; j <= i; j++){
+//                System.out.print(k + " ");
+//                k++;
+//            }
+//            System.out.println();
+//        }
+
+////        0-1 triangle
+//        for (int i = 1; i <= n; i++){
+//            for (int j = 1; j <= i; j++){
+//                if ((i+j) % 2 == 0) {
+//                    System.out.print("1");
+//                } else {
+//                    System.out.print("0");
+//                }
+//            }
+//            System.out.println();
+//        }
+
+
     }
 }
